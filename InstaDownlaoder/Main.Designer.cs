8286a6b97e05cdae7c8662e7983b7a94ff6e0c19
Amir Hossein Roohi <ahroohi1379@hotmail.com>
2019-06-05 21:28:03 +0200

    new color

--- InstaDownlaoder/Main.Designer.cs
+++ InstaDownlaoder/Main.Designer.cs
@@ -45,8 +45,10 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
             this.ClientSize = new System.Drawing.Size(768, 443);
             this.Controls.Add(this.tabPanel);
+            this.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "Main";
             this.Text = "Instagram Downloader - @AmirRoohi2K";
             this.ResumeLayout(false);
